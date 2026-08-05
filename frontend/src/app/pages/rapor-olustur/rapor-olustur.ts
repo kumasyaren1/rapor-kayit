edca47087fb6bdbde4f300a85774914d9940aa1c
kumasyaren1 <yaren5433@gmail.com>
2026-08-05 17:36:55 +0300

    Rapor oluştur ekranı tasarlandı.

---
+++ frontend/src/app/pages/rapor-olustur/rapor-olustur.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rapor-olustur',
+  imports: [],
+  templateUrl: './rapor-olustur.html',
+  styleUrl: './rapor-olustur.scss',
+})
+export class RaporOlustur {}
