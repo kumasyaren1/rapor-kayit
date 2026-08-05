eb868e5be8af02f0949cd5478cb3091d7210e79a
kumasyaren1 <yaren5433@gmail.com>
2026-08-05 16:25:28 +0300

    Sicil controller oluşturuldu.

--- src/main/java/com/example/raporkayit/controller/SicilController.java
+++ src/main/java/com/example/raporkayit/controller/SicilController.java
@@ -1,9 +1,27 @@
 package com.example.raporkayit.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.raporkayit.dto.MukellefResponse; //Yalnızca görünmesini istediğimiz bilgileri taşıyan DTO’yu veriyor.
+import com.example.raporkayit.service.SicilService; //Bulunan entity’yi MukellefResponse DTO’suna çeviriyor.
+import org.springframework.web.bind.annotation.*; //Spring’in web anotasyonlarını kullanabilmemizi sağlar.
 
-@RestController
-@RequestMapping("/api/sicil")
+@RestController //Bu sınıf HTTP isteklerini karşılayan bir REST controller’dır. Metotların döndürdüğü nesneleri JSON’a çevir
+@RequestMapping("/api/sicil") //Bu controller içerisindeki bütün endpointlerin ortak adresini belirler.
 public class SicilController {
+
+    private final SicilService sicilService;//Bu controller çalışabilmek için bir SicilService nesnesine ihtiyaç duyuyor ve onu bu değişkende tutacak
+
+    public SicilController(SicilService sicilService) {
+        this.sicilService = sicilService;//Constructor’a gelen SicilService nesnesini, controller’ın içerisindeki sicilService değişkeninde sakla.
+    }
+
+    @GetMapping("/mukellef") //Bu metodu bir HTTP GET isteğine bağlar.
+    public MukellefResponse mukellefSorgula(
+            @RequestParam(required = false) String vergiKimlikNo,//@RequestParam, URL’de ? işaretinden sonra gönderilen değerleri alır.
+            @RequestParam(required = false) String tcKimlikNo) { // required = false Bu parametrenin tek başına zorunlu olmadığını belirtir
+
+        return sicilService.mukellefSorgula( //Burada controller, aldığı değerleri service’e gönderiyor.
+                vergiKimlikNo,
+                tcKimlikNo
+        );
+    }
 }
