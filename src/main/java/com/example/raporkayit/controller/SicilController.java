6748eb0c6138bb6199f9b390dc1434ef354d13b3
kumasyaren1 <yaren5433@gmail.com>
2026-08-12 10:08:02 +0300

    fix: allow frontend access to registry endpoint

--- src/main/java/com/example/raporkayit/controller/SicilController.java
+++ src/main/java/com/example/raporkayit/controller/SicilController.java
@@ -4,8 +4,10 @@ import com.example.raporkayit.dto.MukellefResponse; //Yalnızca görünmesini is
 import com.example.raporkayit.service.SicilService; // Mükellef sorgulama iş kurallarını çalıştıran servis
 import org.springframework.web.bind.annotation.*; //Spring’in web anotasyonlarını kullanabilmemizi sağlar.
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController //Bu sınıf HTTP isteklerini karşılayan bir REST controller’dır. Metotların döndürdüğü nesneleri JSON’a çevir
 @RequestMapping("/api/sicil") //Bu controller içerisindeki bütün endpointlerin ortak adresini belirler.
+
 public class SicilController {
 
     private final SicilService sicilService;//Bu controller çalışabilmek için bir SicilService nesnesine ihtiyaç duyuyor ve onu bu değişkende tutacak
