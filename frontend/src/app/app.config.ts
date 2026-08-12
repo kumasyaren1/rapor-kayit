f4140c216aa5cdb95ba7065fb3a483808eed88d4
kumasyaren1 <yaren5433@gmail.com>
2026-08-12 10:10:28 +0300

    feat: connect taxpayer lookup to report form

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,7 +2,7 @@ import {
   ApplicationConfig,
   provideBrowserGlobalErrorListeners
 } from '@angular/core';
-
+import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { providePrimeNG } from 'primeng/config';
 import { definePreset } from '@primeuix/themes';
@@ -32,6 +32,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
+    provideHttpClient(),
 
     providePrimeNG({
       theme: {
